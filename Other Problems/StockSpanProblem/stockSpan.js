a828291c5e1974810dfa5715ee47e2f84795de61
green-dot <mattwtaylor@protonmail.com>
2020-03-11 00:07:19 -0400

    Remove comments that werent needed and changed the variable span to stockSpans

--- Other Problems/StockSpanProblem/stockSpan.js
+++ Other Problems/StockSpanProblem/stockSpan.js
@@ -1,22 +1,21 @@
 const stockSpan = (prices) => {
   const stack = []
   stack.peek = () => stack[stack.length - 1];
-  const spans = Array(prices.length).fill(null);
+  const stockSpans = Array(prices.length).fill(null);
 
   for(let currentDay = prices.length - 1; currentDay >= 0;  currentDay--){
-    //remove all lower values, should be <= ?
     while(stack.length && stack.peek().price < prices[currentDay]){
       const futureStock = stack.pop();
-      spans[futureStock.day] = futureStock.day - currentDay;
+      stockSpans[futureStock.day] = futureStock.day - currentDay;
       }
     stack.push(new StockPrice(prices[currentDay], currentDay))
   }
   // clear stack
   while(stack.length){
     const futureStock = stack.pop();
-    spans[futureStock.day] = futureStock.day + 1;
+    stockSpans[futureStock.day] = futureStock.day + 1;
   }
-  return spans
+  return stockSpans
 }
 
 class StockPrice {
